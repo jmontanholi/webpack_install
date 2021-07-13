9e750a613e8b39846ef40c6cae8080d2c2ffdf2a
jmontanholi <jonunes9326@hotmail.com.br>
2021-07-13 13:11:04 -0300

    Finish exercise

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
+  
+  return element;
+}
+
+document.body.appendChild(component());
